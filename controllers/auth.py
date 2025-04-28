2266138195eb0e3ab895b41389cdc037f3bae63d
CharlesLuigiMesa14 <mesacharlesluigi@gmail.com>
2025-04-28 19:37:39 +0800

    Mationg

--- controllers/auth.py
+++ controllers/auth.py
@@ -26,7 +26,7 @@ class UserController:
     def add_user():
         try:
             data = request.get_json()
-            required_fields = ['username', 'password', 'email', 'name', 'public_key']
+            required_fields = ['username', 'password', 'email', 'name']
             for field in required_fields:
                 if field not in data:
                     return jsonify({"error": f"Missing field: {field}"}), 400
